fe9c2bf9f2929cf7ecd1301b8075ded8300b6f7a
cvhen-t <2327506380@qq.com>
2022-02-24 20:38:09 +0800

    菜单背景

--- map/src/App.vue
+++ map/src/App.vue
@@ -1,37 +1,37 @@
 <template>
-  <el-container style="height: 900px; ">
+  <el-container style="height: 900px">
     <div class="isShow">
       <!-- 导航部分 -->
-      <el-menu class="el-menu-vertical-demo" :collapse="isCollapse" router active-text-color="#5f8bd3">
+      <el-menu
+        class="el-menu-vertical-demo"
+        :collapse="isCollapse"
+        router
+        active-text-color="#5f8bd3"
+      >
         <!-- <div  class="Navphoto">
         <img class="topImg" v-if="!isCollapse" src="./assets/backjpg.jpeg" alt="">
         <img v-else   alt="" style="width: 63px;">
       </div> -->
-      <div  v-for="item in routes"
-          :key="item.indexnum" >
-  <el-submenu
-         v-if="!!item.name"
-          :index="item.path"
-        >
-          <template slot="title"
-            ><i :class="item.icon " class="iconNav2"></i
-            >{{ isCollapse ? "" : item.name }} </template
-          >
-          <div v-if="!!item.children">
-          <el-menu-item-group>
-              <el-menu-item
-                v-for="(itemc) in item.children"
-                :key="itemc.indexnum"
-                :index="itemc.path"
+        <div v-for="item in routes" :key="item.indexnum">
+          <el-submenu v-if="!!item.name" :index="item.path">
+            <template slot="title"
+              ><i :class="item.icon" class="iconNav2"></i
+              >{{ isCollapse ? "" : item.name }}
+            </template>
+            <div v-if="!!item.children">
+              <el-menu-item-group>
+                <el-menu-item
+                  v-for="itemc in item.children"
+                  :key="itemc.indexnum"
+                  :index="itemc.path"
                 >
-                  <i class="el-icon-guide   iconNav3" ></i>
-                {{ itemc.name }}</el-menu-item
-              >
-          </el-menu-item-group>
+                  <i class="el-icon-guide iconNav3"></i>
+                  {{ itemc.name }}</el-menu-item
+                >
+              </el-menu-item-group>
             </div>
-        </el-submenu>
-      </div>
-      
+          </el-submenu>
+        </div>
       </el-menu>
       <!-- </el-aside> -->
     </div>
@@ -93,7 +93,6 @@ export default {
   z-index: 1000;
   box-shadow: 3px 0px 7px #888888;
   background-image: url(../src/assets/backjpg.jpeg);
-  
 }
 
 .el-header {
@@ -101,15 +100,15 @@ export default {
   color: #333;
   line-height: 60px;
   border-bottom: 1px solid #ddd;
-  
-  /* background-image: url(../src/assets/jpg.png); */
+  /* box-shadow: 0px 0px 9px #888888 !important; */
 
+  /* background-image: url(../src/assets/jpg.png); */
 }
-.el-menu{
+.el-menu {
   color: rgb(51, 51, 51);
   font-weight: 600;
   min-height: 0px !important;
-  opacity:0.93 !important;
+  opacity: 0.93 !important;
 }
 
 .el-aside {
@@ -126,7 +125,7 @@ export default {
   height: 100%;
   padding: 15px;
   background-color: white;
-  border: 0px ;
+  border: 0px;
 }
 .el-main {
   margin: 0px !important;
@@ -149,19 +148,17 @@ export default {
   font-size: 23px !important;
   line-height: 50px;
 }
-.iconNav2{
+.iconNav2 {
   padding-right: 5px;
   color: #5f8bd3 !important;
   font-size: 22px !important;
-
 }
-.iconNav3{
+.iconNav3 {
   color: #5f8bd3 !important;
-  font-size:8px !important;
-
+  font-size: 8px !important;
 }
 
-.topImg{
+.topImg {
   width: 139px;
   height: 150px;
 }
